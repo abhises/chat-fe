08ed9d3343d3d24b0c4e0bd8c52c3cbb2d2ca05d
abhises <abhisespoudyal@gmail.com>
2025-01-16 21:41:02 +0545

    fixing css error

--- src/components/chat/UserChat.jsx
+++ src/components/chat/UserChat.jsx
@@ -10,23 +10,22 @@ const UserChat = ({ chat, user }) => {
     <Stack
       direction="horizontal"
       gap={3}
-      className="User-card align-items-center p-2 justify-content-between"
+      className="user-card align-items-center p-2 justify-content-between"
       role="button">
       <div className="d-flex">
         <div className="me-2">
           <img src={avatar} height="35px" />
-          <span className="user-online"></span>
         </div>
         <div className="text-content">
           <div className="name">{recipientUser?.name}</div>
           <div className="text">Text Message</div>
         </div>
-      </div>
-      <div className="d-flex flex-column align-items-end">
-        <div className="date">12/12/2022</div>
-        <div className="this-user-notifications">
-          {" "}
-          2 <span className="user-online"></span>
+        <div className="d-flex flex-column align-items-end">
+          <div className="date">12/12/2022</div>
+          <div className="this-user-notifications">
+            {" "}
+            2 <span className="user-online"></span>
+          </div>
         </div>
       </div>
     </Stack>
